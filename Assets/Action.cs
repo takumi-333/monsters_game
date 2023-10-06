94d85abe31cc14795b64cbd7519023b5fbfc3cf3
takumi-333 <takumi_soccer0514@icloud.com>
2023-10-06 14:25:02 +0900

    [add] バトルメッセージの表示機能追加

--- Assets/Action.cs
+++ Assets/Action.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Action
 {
@@ -14,7 +15,29 @@ public class Action
         this.action_id = action_id;
     }
 
-    public virtual void HandleAction() {
+    public int CalDamage() {
+        int p_critical = 5;
+        int r_critical = Random.Range(0, 100);
+        int total_damage = 0;
+        int r;
+        //クリティカルじゃないとき
+        if (r_critical >= p_critical) {
+            int min_damage = Random.Range(0,2);
+            r = Random.Range(90, 110);
+            total_damage = attacker.param.atk / 2 - defender.param.def / 4;
+            total_damage = total_damage * r / 100;
+            if (total_damage <= 0) {
+                total_damage = min_damage;
+            }
+        } else {
+            r = Random.Range(110, 130);
+            total_damage = (attacker.param.atk / 2) * r / 100;
+        }
+
+        return total_damage;
+    }
+
+    public virtual void HandleAction(TextMeshProUGUI battleMessage) {
         Debug.Log("Called Parent HandleAction()");
     }
 }
